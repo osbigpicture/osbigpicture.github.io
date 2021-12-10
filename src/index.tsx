46d1662393d93ed1037533d4b2f43538ce753267
Yeseul O <yeseulo@gmail.com>
2021-12-10 15:42:30 +0900

    feat: add ga4

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "App";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-2BXWJKLQ4R");
+ReactGA.send("pageview");
 
 ReactDOM.render(
   <React.StrictMode>
